a1ea1c7e25a65053763e7e0a4d7de63ac5644656
Francisco Gonzalez Herzberg <86564920+Fran477@users.noreply.github.com>
2021-10-13 16:16:50 -0300

    Add files via upload

--- Controller/categoriasController.php
+++ Controller/categoriasController.php
@@ -1,5 +1,5 @@
 <?php
-require_once "./Model/categoriasModel.php"; require_once "./View/categoriasView.php";
+require_once "./Model/categoriasModel.php"; require_once "./View/categoriasView.php"; require_once "./Helpers/authHelper.php";
 
 class categoriasController{
     
@@ -9,19 +9,42 @@ class categoriasController{
     function __construct(){
         
         $this->model = new categoriasModel();
-        $this->view = new categoriasView();}
+        $this->view = new categoriasView();
+        $this->authHelper = new AuthHelper();}
 
     function categorias(){
         $categorias = $this->model->getCategorias();
         
         $this->view->mostrarCategorias($categorias);}
     
-    }
+    
 
-    //<---no funciona---->
-    // function agregarCategorias(){
+  
+     function agregarCategorias(){
         
-    //     $this->authHelper->checkloggedIn();
-    //     $this->model->agregarCategorias($_POST['id_categoria'], $_POST['nombre'],$_POST['descripcion']);
-    //     $this->view->redirigirAdministracion();
-    // }
+        $this->authHelper->checkloggedIn();
+        $this->model->agregarCategorias($_POST['nombre'],$_POST['descripcion']);
+        $this->view->redirigirAdministracion(); }
+    
+    
+    function FormularioEditarCategoria($id){
+        
+            $this->authHelper->checkloggedIn();
+            $categorias = $this->model->getCategoria($id);
+            $this->view->FormularioEditarCategoria($id, $categorias);}
+    
+    function editarCategoria($id_categoria){
+            
+            $this->authHelper->checkloggedIn();
+            $this->model->editarCategoria($_POST['nombre'], $_POST['descripcion'], $id_categoria);
+            $this->view->redirigirAdministracion();}
+    
+  //<-----------Solo deja borrar categorias que no tengan productos asociados------>
+//<-----------falta arreglar-------------->   
+    function borrarCategoria($id){
+        
+                $this->authHelper->checkloggedIn();
+                $this->model->borrarCategoria($id);
+                $this->view->redirigirAdministracion();}
+}          
+        
